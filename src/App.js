5947bdbb34e17263c1499a077b9a28bcebb966bb
AKavkaz <andrewkhomoutov@hotmail.com>
2024-04-21 23:19:42 +0800

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Menu from './components/Menu';
+import HeaderBlock from './components/HeaderBlock';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
+    <Router>
     <div className="App">
-      <h1>Tesla Clone</h1>
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+      {isMenuOpen && <Menu />}
+      <HeaderBlock/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/HeaderBlock.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./HeaderBlock.css";
+
+export class HeaderBlock extends Component {
+  render() {
+    return (
+      <div className="headerBlock">
+        <div className="headerBlock__info">
+          <div className="headerBlock__info--text">
+            <h1>Model 3</h1>
+            <h4>
+              Order Online for <span>Touchless Delivery</span>
+            </h4>
+          </div>
+          <div className="headerBlock__actions">
+            <button className="headerBlock__buttonPrimary">custom order</button>
+            <button className="headerBlock__buttonSecondary">existing inventory</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeaderBlock;
